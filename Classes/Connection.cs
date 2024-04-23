4500ef8731b2acac379ef4a61d1d7e3aa941a420
MatveiKylosov <matveikyl@mail.ru>
2024-04-23 17:27:20 +0500

    Разработанные классы общего пользования. Kylosov

---
+++ Classes/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace ShopContent.Classes
+{
+    class Connection
+    {
+        private static readonly string config = "server = localhost;Trusted_Connection=No;DataBase=ShopContent;User=***;PWD=***";
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
