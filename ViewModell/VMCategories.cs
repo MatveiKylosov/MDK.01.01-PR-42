85695d6992c3f8930b0f61a338d0e950a5e46f0b
MatveiKylosov <matveikyl@mail.ru>
2024-04-23 22:31:00 +0500

    Разработанные модели представления

---
+++ ViewModell/VMCategories.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace ShopContent.ViewModell
+{
+    public class VMCategories : INotifyPropertyChanged
+    {
+        public ObservableCollection<Context.CategoriesContext> Categories { get; set; }
+        public VMCategories() => Categories = Context.CategoriesContext.AllCategories();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
